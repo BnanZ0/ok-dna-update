5e8bbebb512be1abc8c6afe175db9856622da5fa
RU <110257560+BnanZ0@users.noreply.github.com>
2025-11-15 13:15:30 +0000

    新增全自动探险（移动逻辑来自群友实现） (medicinemelancholy)

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 from ok import ConfigOption
 
-version = "v1.0.27"
+version = "v1.0.28"
 #不需要修改version, Github Action打包会自动修改
 
 key_config_option = ConfigOption('Game Hotkey Config', { #全局配置示例
@@ -133,6 +133,7 @@ config = {
         ["src.tasks.AutoFishTask", "AutoFishTask"],
         ["src.tasks.AutoSkill", "AutoSkill"],
         ["src.tasks.AutoExpulsion", "AutoExpulsion"],
+        ["src.tasks.AutoExploration_Fast", "AutoExploration_Fast"],
         ["src.tasks.AutoEscortTask", "AutoEscortTask"],
         ["src.tasks.Auto65ArtifactTask_Fast", "Auto65ArtifactTask_Fast"],
         ["src.tasks.Auto70jjbTask", "Auto70jjbTask"],
